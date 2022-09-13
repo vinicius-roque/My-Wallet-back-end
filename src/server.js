6ee881b29b130be8383d6a8b0dcbf228e27e9586
Vinícius Roque <viniciusfr6@gmail.com>
2022-09-13 11:11:05 -0300

    chore: adjust code for deploy on heroku

--- src/server.js
+++ src/server.js
@@ -2,6 +2,9 @@ import express  from "express";
 import cors from 'cors';
 import authRouter from './routes/authRouter.js';
 import transactionsRouter from './routes/transactionsRouter.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const server = express();
 server.use(express.json());
@@ -10,4 +13,4 @@ server.use(cors());
 server.use(authRouter);
 server.use(transactionsRouter);
 
-server.listen(5000, () => console.log("Listening on port 5000"));
+server.listen(process.env.PORT, () => console.log("Server running on port " + process.env.PORT));
